d8653caa9c136e7b55e48738e0b7f82d33bd94f8
Jim Bosch <jbosch@astro.princeton.edu>
2014-04-25 18:14:11 -0400

    Fix bugs in makeShearCatalog.py

--- python/lsst/obs/great3/makeShearCatalog.py
+++ python/lsst/obs/great3/makeShearCatalog.py
@@ -34,7 +34,12 @@ class ReductionTaskRunner(lsst.pipe.base.TaskRunner):
 
     @staticmethod
     def getTargetList(parsedCmd, **kwargs):
-        return [(parsedCmd.butler, parsedCmd.id.refList,)]
+        return [(parsedCmd.butler, parsedCmd.id.refList, kwargs)]
+
+    def __call__(self, args):
+        butler, dataRefList, kwargs = args
+        task = self.makeTask(args=args)
+        task.run(butler, dataRefList, **kwargs)
 
 class MakeShearCatalogConfig(lsst.pex.config.Config):
     useWeights = lsst.pex.config.Field(
@@ -42,19 +47,24 @@ class MakeShearCatalogConfig(lsst.pex.config.Config):
         doc="Whether to use per-object SNR weights in shear estimation",
         default=True,
         )
-    distortionField = lsst.pex.conofig.Field(
+    maxDistortion = lsst.pex.config.Field(
+        dtype=float,
+        doc="Clip measurements with |e| greater than this value",
+        default=2.0
+        )
+    distortionField = lsst.pex.config.Field(
         dtype=str,
         doc=("Prefix for field containing distortion values in .e1, .e2, and error in .err"),
         default="shape.hsm.regauss",
         )
     useImprovedSigmaFactor = lsst.pex.config.Field(
         dtype=bool,
-        doc="Whether to use the improved 2/(1+2*sigma_g^2) factor from BJ02 in converting sigma_g to sigma_e"
+        doc="Whether to use the improved 2/(1+2*sigma_g^2) factor from BJ02 in converting sigma_g to sigma_e",
         default=True,
         )
     useImprovedResponsivity = lsst.pex.config.Field(
         dtype=bool,
-        doc="Whether to use the improved responsivity factor from BJ02 (eqn 5-33) in distortion to shear"
+        doc="Whether to use the improved responsivity factor from BJ02 (eqn 5-33) in distortion to shear",
         default=False,
         )
     shearCalibrationFactor = lsst.pex.config.Field(
@@ -71,7 +81,7 @@ class MakeShearCatalogConfig(lsst.pex.config.Config):
 
 class MakeShearCatalogTask(lsst.pipe.base.CmdLineTask):
 
-    RunnerClass = ReductionRunner
+    RunnerClass = ReductionTaskRunner
     ConfigClass = MakeShearCatalogConfig
 
     _DefaultName = "makeShearCatalog"
@@ -87,9 +97,9 @@ class MakeShearCatalogTask(lsst.pipe.base.CmdLineTask):
                                               doc="corrected error on distortion ('sigma_e')")
         self.g1Key = self.schema.addField("g1", type=float, doc="axis-aligned shear component")
         self.g2Key = self.schema.addField("g2", type=float, doc="off-axis shear component")
-        if self.config.useWeights:
-            self.weightKey = self.schema.addField("weight", type=float,
-                                                  doc="per-object weight to apply when combining shears")
+        self.badKey = self.schema.addField("bad", type=int, doc="nonzero if the measurement is bad")
+        self.weightKey = self.schema.addField("weight", type=float,
+                                              doc="per-object weight to apply when combining shears")
 
     def run(self, butler, dataRefList):
         # Combine the catalogs from the different subfields, copying only the relevant values
@@ -97,21 +107,34 @@ class MakeShearCatalogTask(lsst.pipe.base.CmdLineTask):
         outCat = lsst.afw.table.BaseCatalog(self.schema)
         outCat.reserve(10000*len(dataRefList))
         for dataRef in dataRefList:
-            self.log.info("Reading catalog for subfield %d" % dataRef.dataId["subfield"])
             inCat = dataRef.get("src", immediate=True)
             if mapper is None:
-                flagKey = inCat.schema.find(self.config.distortionField + ".flags").key
+                flagKeyIn = inCat.schema.find(self.config.distortionField + ".flags").key
+                e1KeyIn = inCat.schema.find(self.config.distortionField + ".e1").key
+                e2KeyIn = inCat.schema.find(self.config.distortionField + ".e2").key
+                errKeyIn = inCat.schema.find(self.config.distortionField + ".err").key
                 mapper = lsst.afw.table.SchemaMapper(inCat.schema, outCat.schema)
                 mapper.addMapping(inCat.schema.find("id").key, "id", True)
-                mapper.addMapping(inCat.schema.find(self.config.distortionField + ".e1").key, "e1", True))
-                mapper.addMapping(inCat.schema.find(self.config.distortionField + ".e2").key, "e2", True))
-                mapper.addMapping(inCat.schema.find(self.config.distortionField + ".err").key, "err", True))
+                mapper.addMapping(e1KeyIn, "e1", True)
+                mapper.addMapping(e2KeyIn, "e2", True)
+                mapper.addMapping(errKeyIn, "err", True)
                 assert mapper.getOutputSchema() == self.schema
-            outCat.extend(inCat[numpy.logical_not(inCat.get(flagKey))], mapper=mapper)
+            tmpCat = lsst.afw.table.BaseCatalog(outCat.table)
+            tmpCat.extend(inCat, mapper=mapper)
+            e = (tmpCat.get(self.e1Key)**2 + tmpCat.get(self.e2Key)**2)**0.5
+            eBad = numpy.logical_not(e < self.config.maxDistortion)
+            tmpCat[self.badKey][:] = numpy.logical_or(eBad, inCat.get(flagKeyIn))
+            outCat.extend(tmpCat, deep=False)
+            self.log.info("Subfield %d; %d bad (%d flagged, %d had |e| too large)"
+                          % (dataRef.dataId["subfield"], tmpCat[self.badKey].sum(),
+                             inCat.get(flagKeyIn).sum(), eBad.sum()))
+
+        badMask = outCat.get(self.badKey).astype(bool)
+        goodMask = numpy.logical_not(badMask)
 
         # Correct uncertainties to be applicable to distortion, not shear
         self.log.info("Computing per-object distortion uncertainty")
-        if self.config.useImproveSigmaFactor:
+        if self.config.useImprovedSigmaFactor:
             factor = 2.0 / (1.0 + 2.0*outCat[self.errKey]**2)
         else:
             factor = 2.0
@@ -119,12 +142,17 @@ class MakeShearCatalogTask(lsst.pipe.base.CmdLineTask):
 
         # Iteratively compute RMS weights and ellipticity (note that default is just one iteration)
         def computeRMS(e1, e2, eSigma, w):
-            return numpy.sum(0.5*(e1**2 + e2**2 - 2*eSigma)*w)
-        rms = computeRMS(outCat[self.e1Key], outCat[self.e2Key], outCat[self.eSigmaKey], 1.0 / len(outCat))
+            return numpy.sqrt(numpy.sum((0.5*(e1**2 + e2**2 - 2*eSigma**2)*w)[goodMask]))
+        outCat[self.weightKey][:] = 1.0
+        outCat[self.weightKey][badMask] = 0.0
+        outCat[self.weightKey][:] /= numpy.sum(outCat[self.weightKey][goodMask])
+        rms = computeRMS(outCat[self.e1Key], outCat[self.e2Key], outCat[self.eSigmaKey],
+                         outCat[self.weightKey])
         self.log.info("Unweighted distortion RMS=%s" % rms)
         if self.config.useWeights:
             outCat[self.weightKey][:] = 1.0 / (rms**2 + outCat[self.eSigmaKey]**2)
-            outCat[self.weightKey][:] /= numpy.sum(outCat[self.weightKey])
+            outCat[self.weightKey][badMask] = 0.0
+            outCat[self.weightKey][:] /= numpy.sum(outCat[self.weightKey][goodMask])
             for i in range(self.config.nRmsIterations):
                 rms = computeRMS(outCat[self.e1Key], outCat[self.e2Key], outCat[self.eSigmaKey],
                                  outCat[self.weightKey])
@@ -142,6 +170,10 @@ class MakeShearCatalogTask(lsst.pipe.base.CmdLineTask):
         f = self.config.shearCalibrationFactor / responsivity
         outCat[self.g1Key][:] = f*outCat[self.e1Key]
         outCat[self.g2Key][:] = f*outCat[self.e2Key]
+
+        # Set flagged measurements to a large value so the presubmission script will ignore them
+        outCat[self.g1Key][badMask] = 100.0
+        outCat[self.g2Key][badMask] = 100.0
 
         # Write outputs
         butler.put(outCat, "shear")
